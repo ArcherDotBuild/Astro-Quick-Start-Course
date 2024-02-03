24980ecc11be461ab6b979c2ea83f918b0572fbb
ElfGod <albertogx@icloud.com>
2024-02-02 21:33:45 -0500

    Collection Schema

--- orbital-osiris/src/content/config.ts
+++ orbital-osiris/src/content/config.ts
@@ -1,5 +1,16 @@
-import { defineCollection, getCollection } from 'astro:content'
-const blogCollection = defineCollection({})
+import { string } from 'astro/zod'
+import { defineCollection, z } from 'astro:content'
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    author: z.
+    string(),
+    image: z.string(),
+    tags: z.array(z.string())
+  })
+})
 
 export const collections = {
   blog: blogCollection,
